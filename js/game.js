5fb089f11a680eb5f053efa6430718351889efde
dayanuyim <dayanuyim@gmail.com>
2017-07-08 12:01:37 +0800

    course this morning

--- js/game.js
+++ js/game.js
@@ -5,35 +5,43 @@
 * Time: 02:32 AM
 * To change this template use Tools | Templates.
 */
-function start() {
-    var canvas = document.getElementById("draw");
 
-    var ctx = canvas.getContext("2d");
+(function(){
 
-    var background = Sizzle('#game')[0],
-        rect = background.getBoundingClientRect();
+    function Start() {
+        var canvas = $("#draw")[0];
 
-    canvas.width = rect.width;
-    canvas.height = rect.width;
+        var ctx = canvas.getContext("2d");
 
-    maxWidth = rect.width;
-    maxHeight = rect.height;
-    maxR = (rect.height > rect.width) ? rect.width / 2 : rect.height / 2;
+        var background = Sizzle('#game')[0],
+            rect = background.getBoundingClientRect();
 
-    ballX = Math.floor(Math.random() * maxWidth);
-    ballY = Math.floor(Math.random() * maxHeight);
-    ballR = Math.floor(Math.random() * maxR) + 30;
+        canvas.width = rect.width;
+        canvas.height = rect.width;
 
-    ctx.fillStyle = 'black';
-    ctx.beginPath();
-    ctx.arc(ballX, ballY, ballR, 0, Math.PI * 2, true);
-    ctx.fill();
+        maxWidth = rect.width;
+        maxHeight = rect.height;
+        //maxR = (rect.height > rect.width) ? rect.width / 2 : rect.height / 2;
+        maxR = Math.min(rect.height, rect.width) / 2;
 
-    document.getElementById("cover")
-            .addEventListener("click", touchEvent, false);
+        ballX = Math.floor(Math.random() * maxWidth);
+        ballY = Math.floor(Math.random() * maxHeight);
+        ballR = Math.floor(Math.random() * maxR) + 30;
 
-    console.log('Start Game');
-}
+        ctx.fillStyle = 'red';
+        ctx.beginPath();
+        ctx.arc(ballX, ballY, ballR, 0, Math.PI * 2, true);
+        ctx.fill();
+
+        document.getElementById("game")
+                .addEventListener("click", function(evt){
+                    console.log('clicked: ' + evt.clientX + ", " + evt.clientY);
+                }, false);
+
+        console.log('Start Game');
+    }
 
 
-setInterval(function () { start() }, 1000);
+    setInterval(function () { Start() }, 1000);
+
+})();
